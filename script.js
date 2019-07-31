5346c42f04215469a12bae29c515ede13d9cc08c
nobresxe1 <rgs190385@gmail.com>
2019-07-30 23:07:57 -0300

    continua o jogo dos numeros

--- script.js
+++ script.js
@@ -42,18 +42,16 @@ function conferirPalpite() {
         campoPalpite.value = '';
         campoPalpite.focus();
 
+
     }
-
-
-
-
-
-
-
-
-
-
-
 }
+envioPalpite.addEventListener('click', conferirPalpite);
 
-
+function configFimDeJogo() {
+    campoPalpite.disabled = true;
+    envioPalpite.disabled = true;
+    botaoReinicio = document.createElement('button');
+    botaoReinicio.textContent = 'Iniciar novo jogo';
+    document.body.appendChild(botaoReinicio);
+    botaoReinicio.addEventListener('click', reiniciarJogo);
+}
